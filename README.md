786326892b72c786167d8686e00cb49ec6795042
Emmanuel Asante <emmaasante676@gmail.com>
2024-03-04 15:46:46 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# About Project
+
+This is a clone of the [Podia](https://www.podia.com/digital-products) Website
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App(){
+  return (
+    <div>
+      Hello, Monica!
+    </div>
+    
+  )
+}
+
+export default App;
