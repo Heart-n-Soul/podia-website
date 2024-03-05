f997a8619e2d67d079f1b5f1b90ab4624e4d2697
Emmanuel Asante <emmaasante676@gmail.com>
2024-03-05 14:09:47 +0000

    build(style): add tailwind css to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,3 @@
-function App(){
-  return (
-    <div>
-      Hello, Monica!
-    </div>
-    
-  )
+export default function App() {
+  return <div className="text-xl">Hello Podia!</div>;
 }
-
-export default App;
