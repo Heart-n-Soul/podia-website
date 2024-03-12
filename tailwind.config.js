e357ac312d4f2c56a342c678a83e170a12179b24
Emmanuel Asante <emmaasante676@gmail.com>
2024-03-12 15:31:42 +0000

    build(tailwind.config.js): add colors and font to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors:{
+      'purple': '#4b2aad',
+      'white': '#ffffff',
+      'off-white': '#fdfbf9',
+      'soft-purple': '#9484c6',
+      'dark-purple': '#201445',
+      'footer-bg-cl': '#f5efeb',
+    },
+    fontFamily: {
+      'inter': ['Inter', 'sans-serif'],
+      'tiempos': ['tiempos', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
