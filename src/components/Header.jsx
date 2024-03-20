c900322aac07294b86149d8ffb11a17261b35739
Emmanuel Asante <emmaasante676@gmail.com>
2024-03-20 02:43:33 +0000

    feat(src): add UI components of page and header comp structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <h1>podia</h1>
+        <ul>
+          <li>Website</li>
+          <li>Email</li>
+          <li>Sell</li>
+          <li>Pricing</li>
+          <li>Resources</li>
+        </ul>
+      </nav>
+      <div>
+        <a href="">Login</a>
+        <a href="">Sign up free</a>
+      </div>
+    </header>
+  );
+}
