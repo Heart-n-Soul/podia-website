8bce6c6333ab216b66b1ddbdaa98de107c57a346
Emmanuel Asante <emmaasante676@gmail.com>
2024-03-20 02:49:32 +0000

    build(fluid-typography): add tailwind config for fluid typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors:{
-      'purple': '#4b2aad',
-      'white': '#ffffff',
-      'off-white': '#fdfbf9',
-      'soft-purple': '#9484c6',
-      'dark-purple': '#201445',
-      'footer-bg-cl': '#f5efeb',
+    colors: {
+      purple: "#4b2aad",
+      white: "#ffffff",
+      "off-white": "#fdfbf9",
+      "soft-purple": "#9484c6",
+      "dark-purple": "#201445",
+      "footer-bg-cl": "#f5efeb",
     },
     fontFamily: {
-      'inter': ['Inter', 'sans-serif'],
-      'tiempos': ['tiempos', 'serif'],
+      inter: ["Inter", "sans-serif"],
+      tiempos: ["tiempos", "serif"],
+    },
+    fluidTypography: {
+      remSize: 14,
+      minScreenSize: 375,
+      maxScreenSize: 1280,
+      minTypeScale: 1.250,
+      maxTypeScale: 1.618,
+      lineHeight: 1.5
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [require("tailwind-fluid-typography")],
 };
