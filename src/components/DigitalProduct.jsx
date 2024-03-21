3f1f89a2840e1d1e925087a4d0eb05d600ca269a
Emmanuel Asante <emmaasante676@gmail.com>
2024-03-21 22:10:36 +0000

    feat(DigitalProduct.jsx): add DigitalProduct component structure

---
+++ src/components/DigitalProduct.jsx
@@ -0,0 +1,25 @@
+export default function DigitalProduct(){
+    return(
+        <section>
+            <div>
+                <p>Digital Products</p>
+                <h2>
+                    Sell online courses, coaching sessions, webinars, workshops,
+                    ebooks, and anything you can think of on Podia
+                </h2>
+                <p>
+                    Get everything you need in one place that you own. Podia takes care of hosting 
+                    products to your customers. And you always control your own work.
+                </p>
+                <a href="#">
+                    Get your free account
+                </a>
+            </div>
+            <div>
+                <figure>
+                    <img src="" alt="Product Image" />
+                </figure>
+            </div>
+        </section>
+    )
+}
